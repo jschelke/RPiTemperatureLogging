b4b73a871889b1998146ad85610eb392052e8b95
jschelke <jeroen.schelkens@telenet.be>
2018-05-31 11:46:52 +0200

    test

--- gitBackup.sh
+++ gitBackup.sh
@@ -1,11 +1,5 @@
 git add .
-
-#echo 'Enter the commit message:'
-$commitMessage = date
-
-git commit -m "$commitMessage"
-
-#echo 'Enter the name of the branch:'
-read branch
-
-git push origin master
+#commitMessage = `date +%Y-%m-%d`
+#echo $commitMessage
+git commit -am "test"
+git push
