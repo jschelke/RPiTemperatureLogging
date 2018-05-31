9262911e9f7f2e6c2690069a17a50e1e27e3198b
jschelke <jeroen.schelkens@telenet.be>
2018-05-31 11:51:38 +0200

    test

--- gitBackup.sh
+++ gitBackup.sh
@@ -2,5 +2,4 @@ git add .
 #commitMessage = `date +%Y-%m-%d`
 #echo $commitMessage
 git commit -am "test"
-git remote add origin https://github.com/jschelke/RPiTemperatureLogging.git
-git push origin master
+git push --all
